a541b2f2e2dfb17405a2f454b5ef9da2ba8a50cf
geovanavillafranca <geovanavillafranca@hotmail.com>
2021-09-05 14:41:35 -0300

    Criado tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import Evento
 
-# Register your models here.
+# Register your models here.S
+
+
+class EventoAdmin(admin.ModelAdmin):
+    # quais campos quero que apareca
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario', 'data_evento',)
+
+# essa classe Evento registra EventoAdmin
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+                    # tipo do campo e seu limite
+    titulo = models.CharField(max_length=100)
+                        # quer dizer que pode ficar em branco e igual a nulo
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    # esse dato tem que ser automatico
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
